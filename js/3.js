409e12ab90661da7f9463b4d8e1f141b5e61b496
Xinrui Chen <xyn.hello@gmail.com>
2022-12-11 21:36:27 -0800

    aoc day3 js

---
+++ js/3.js
@@ -0,0 +1,32 @@
+let fs = require("fs")
+
+const sum = (a, b) => a + b
+
+const read = (str) => fs.readFileSync(str).toString().split("\n") // get data
+const convToNum = (char) =>
+  char.codePointAt(0) > 96 ? char.codePointAt(0) - 96 : char.codePointAt(0) - 38
+
+const splitTwo = (str) => {
+  const half = Math.floor(str.length / 2)
+  return [str.slice(0, half), str.slice(half, str.length)]
+}
+const splitThree = (res, line, i) => {
+  i % 3 !== 0 && res.length > 0 ? res[res.length - 1].push(line) : res.push([line])
+  return res
+}
+
+const findMatch = (args) => {
+  let match
+  Array.from(args[0]).forEach((char) => {
+    if (args.slice(1).every((str) => [...str].includes(char))) {
+      match = char
+      return
+    }
+  })
+  return match
+}
+
+const data = read("../src/3.txt")
+const partOne = data.map(splitTwo).map(findMatch).map(convToNum).reduce(sum, 0)
+const partTwo = data.reduce(splitThree, []).map(findMatch).map(convToNum).reduce(sum, 0)
+console.log("Part 1 Sum:", partOne, "\n", "Part 2 Sum:", partTwo)
