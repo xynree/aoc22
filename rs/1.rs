b48c86052f10f31df7a8f03c23f6be502760ef0d
Xinrui Chen <xyn.hello@gmail.com>
2022-12-30 21:20:41 -0800

    slight edit

--- rs/1.rs
+++ rs/1.rs
@@ -2,14 +2,13 @@ use std::fs;
 
 fn main() {
     let data = fs::read_to_string("../../src/1.txt").expect("To read file");
-    let lines = data.lines();
 
-    let mut highest = lines.fold(Vec::new(), |mut a, b| {
+    let mut highest = data.lines().fold(Vec::new(), |mut a, b| {
         if b == "" || a.len() == 0 {
             a.push(0);
         } else {
             let c = a.last_mut().expect("Should be a number");
-            *c += b.parse::<i32>().expect("Should be a number");
+            *c += b.parse::<u32>().expect("Should be a number");
         }
         return a;
     });
@@ -18,5 +17,5 @@ fn main() {
     highest.reverse();
 
     println!("Part 1: {}", highest[0]);
-    println!("Part 2: {}", highest[0] + highest[1] + highest[2])
+    println!("Part 2: {}", highest.iter().take(3).sum::<u32>())
 }
